7e2678b40d2f998545697920cd74b4c341c73144
Chris John <csjohn@shaw.ca>
2023-01-10 16:08:05 -0500

    adding modal

--- src/components/admin/Dashboard/JoinRoundButton.tsx
+++ src/components/admin/Dashboard/JoinRoundButton.tsx
@@ -1,15 +1,59 @@
+import { useState } from "react";
 import Add from "public/images/svg/Add";
 import Button from "src/components/Button";
+import Backdrop from "@mui/material/Backdrop";
+import Box from "@mui/material/Box";
+import Modal from "@mui/material/Modal";
+import Fade from "@mui/material/Fade";
+import Typography from "@mui/material/Typography";
+
+const style = {
+  position: "absolute" as "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 793,
+  bgcolor: "white",
+  boxShadow: 24,
+  p: 4,
+  borderRadius: "9.31292px",
+};
 
 const JoinRoundButton = () => {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
   return (
     <div>
       <Button
         secondary
         text="Join Round"
         icon={<Add className="mb-1 fill-[#898888]" />}
-        onClick={() => console.log("Join Round")}
+        onClick={handleOpen}
       />
+      {/* <Button onClick={handleOpen}>Open modal</Button> */}
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        open={open}
+        onClose={handleClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
+      >
+        <Fade in={open}>
+          <Box sx={style}>
+            <Typography id="transition-modal-title" variant="h6" component="h2">
+              Text in a modal
+            </Typography>
+            <Typography id="transition-modal-description" sx={{ mt: 2 }}>
+              Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+            </Typography>
+          </Box>
+        </Fade>
+      </Modal>
     </div>
   );
 };
