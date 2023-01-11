bd991f3e5f89eead681a55403bd6d27d0de8ae22
Chris John <csjohn@shaw.ca>
2023-01-11 14:07:45 -0500

    adding checkbox to modal

--- src/components/admin/Dashboard/JoinRoundModal.tsx
+++ src/components/admin/Dashboard/JoinRoundModal.tsx
@@ -26,6 +26,7 @@ type JoinRoundModalProps = {
 
 const JoinRoundModal = ({ handleClose, open }: JoinRoundModalProps) => {
   const [inputs, setInputs] = useState({});
+  const [checked, setChecked] = useState(false);
 
   const handleChange = (event: any) => {
     const name = event.target.name;
@@ -75,6 +76,17 @@ const JoinRoundModal = ({ handleClose, open }: JoinRoundModalProps) => {
                 onChange={handleChange}
               />
             </label>
+            <label className="my-2 flex flex-col" htmlFor="emailCheck">
+              Use email associated with Github account?
+              <input
+                className="mr-auto mt-3 text-[#E5E7EB] border-[#E5E7EB] rounded-lg"
+                type="checkbox"
+                name="emailCheck"
+                checked={checked}
+                onChange={() => setChecked((prev) => !prev)}
+              />
+            </label>
+
             <label className="mb-2 mt-5" htmlFor="email">
               Enter email address:
               <br />
@@ -91,6 +103,7 @@ const JoinRoundModal = ({ handleClose, open }: JoinRoundModalProps) => {
                 onChange={handleChange}
               />
             </label>
+
             <input
               className="transition-colors duration-200 ease-in-out transform outline-none focus:outline-none flex flex-row items-center justify-center rounded-md font-bold mx-auto
                 px-6 py-1 border border-[#156ff7] bg-[#156ff7] hover:bg-[#002256] hover:border-[#002256] focus:bg-[#002256] text-white text-lg cursor-pointer my-8"
