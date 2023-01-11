773a448b60c488937283d49a266d5e141650ca22
Chris John <csjohn@shaw.ca>
2023-01-11 16:04:49 -0500

    adding username to auth nav and adding chevron 'svg file

--- src/components/navBar/AuthNavbar.tsx
+++ src/components/navBar/AuthNavbar.tsx
@@ -2,8 +2,8 @@ import { useState, useEffect, useRef } from "react";
 import Fade from "@mui/material/Fade";
 import { useSession, useSupabaseClient } from "@supabase/auth-helpers-react";
 import PLLogo from "public/images/svg/PLLogo";
-import LogoutButton from "../LogoutButton";
 import Navbar from "./Navbar";
+import DownChevron from "public/images/svg/DownChevron";
 
 type CapturedEvent = MouseEvent | TouchEvent;
 
@@ -59,27 +59,15 @@ const AuthNavbar = () => {
                 className="inline-flex w-full justify-center px-4 py-2 text-sm font-medium text-gray-700 "
                 onClick={() => setToggle((prev) => !prev)}
               >
-                User name
-                <svg
-                  className="-mr-1 ml-2 h-5 w-5"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                  aria-hidden="true"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
-                    clipRule="evenodd"
-                  />
-                </svg>
+                {username}
+                <DownChevron />
               </button>
             </div>
             <Fade in={toggle}>
               <div className="absolute px-4 right-0 z-10 mt-2 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                 <div className="py-1">
                   <button
-                    className="block w-full px-4 py-2 text-left text-sm"
+                    className="block w-full px-4 py-2 text-left text-sm min-w-[85px]"
                     onClick={() => handleSignOut()}
                   >
                     Sign out
