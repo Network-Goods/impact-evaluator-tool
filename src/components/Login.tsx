a7d9c544b9ef8191548b9b6e592fb10a66c0af1d
Jason Moggridge <j@jmoggr.com>
2023-01-11 09:24:53 -0500

    Redirect to current route after login

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,13 +1,22 @@
 import { Auth, ThemeSupa } from "@supabase/auth-ui-react";
 import { useSupabaseClient } from "@supabase/auth-helpers-react";
+import { useRouter } from "next/router";
 
 export default function Login() {
   const supabase = useSupabaseClient();
+  const router = useRouter();
+
+  // window is only defined on the client
+  let redirectTo: string | undefined;
+  if (typeof window !== "undefined") {
+    redirectTo =
+      window.location.protocol + "//" + window.location.host + router.route;
+  }
 
   return (
     <>
       <Auth
-        // redirectTo="http://localhost:3001/"
+        redirectTo={redirectTo}
         appearance={{ theme: ThemeSupa }}
         // view="update_password"
         supabaseClient={supabase}
