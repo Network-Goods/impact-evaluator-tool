1b8db65421f91af3ef325328f4ffd5beaa3b6375
Jason Moggridge <github_hou1@jmoggr.com>
2023-03-02 10:57:04 -0500

    Use email for test auth

--- e2e/auth.setup.ts
+++ e2e/auth.setup.ts
@@ -1,26 +1,20 @@
-// auth.setup.ts
 import { test as setup } from "@playwright/test";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-const username = process.env.GITHUB_USERNAME || "";
-const password = process.env.GITHUB_PASSWORD || "";
-
 const authFile = "playwright/.auth/user.json";
 
 setup("authenticate", async ({ page }) => {
-  // Perform authentication steps. Replace these actions with your own.
-  console.log("Authenticating...", username);
-  console.log("Authenticating...", password);
-  await page.goto("http://localhost:3000");
-  await page.getByRole("button", { name: "Sign in with Github" }).click();
-  await page.getByLabel("Username or email address").click();
-  await page.getByLabel("Username or email address").fill(username);
-  await page.getByLabel("Password").click();
-  await page.getByLabel("Password").fill(password);
-  await page.getByRole("button", { name: "Sign in" }).click();
-  // End of authentication steps.
+  await page.goto("/");
+  await page.getByPlaceholder("Your email address").click();
+  await page.getByPlaceholder("Your email address").fill("j@jmoggr.com");
+  await page.getByPlaceholder("Your password").click();
+  await page.getByPlaceholder("Your password").fill("123456");
+  await page.getByRole("button", { name: "Sign in", exact: true }).click();
+
+  // Delay is require for log in to complete before cookies can be correctly saved
+  await page.waitForTimeout(1000);
 
   await page.context().storageState({ path: authFile });
 });
