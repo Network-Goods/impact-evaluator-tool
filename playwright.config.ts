06095aac43693db4fc46d6215faa72d8a41c9119
Jason Moggridge <github_hou1@jmoggr.com>
2023-03-02 16:41:21 -0500

    Debug playwright actions

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,7 +3,6 @@ import { defineConfig, devices } from "@playwright/test";
 import config from "./loadconfig";
 
 console.log("config:  ", config);
-console.log(process.env);
 
 /**
  * See https://playwright.dev/docs/test-configuration.
