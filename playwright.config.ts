5ce61a531f2b2ccc6b56d3b6cc7ed0b53ab08cec
Jason Moggridge <github_hou1@jmoggr.com>
2023-03-02 21:15:13 -0500

    debug

--- playwright.config.ts
+++ playwright.config.ts
@@ -8,7 +8,7 @@ import config from "./loadconfig";
 export default defineConfig({
   testDir: "./e2e",
   /* Maximum time one test can run for. */
-  timeout: 6 * 1000,
+  timeout: 5 * 1000,
   expect: {
     /**
      * Maximum time expect() should wait for the condition to be met.
