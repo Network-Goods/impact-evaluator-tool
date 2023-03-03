29108843e3328731abb323109e62d8b91e4d8c69
Jason Moggridge <github_hou1@jmoggr.com>
2023-03-03 11:36:05 -0500

    debug

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,6 +2,8 @@ import { defineConfig, devices } from "@playwright/test";
 
 import config from "./loadconfig";
 
+console.log("client backend: ", config.CLIENT_BACKEND, !!config.CLIENT_BACKEND);
+
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
