d155764f7c549601faf80c5737c0251ada53271c
Jason Moggridge <github_hou1@jmoggr.com>
2023-03-16 12:40:49 -0400

    Fix issue in generated seed file
    See script change for details.

--- sync-supabase-data.sh
+++ sync-supabase-data.sh
@@ -13,6 +13,10 @@ for f in $(ls "$migrations_dir" | grep -E "[0-9]{14}_seed.sql"); do
   rm "$migrations_dir/$f"
 done
 
+# We filter out lines setting search_path because it breaks foreign keys (I'm not sure how/why)
+# This is why it was added, it is technically a security risk but we trust the database that we're dumping from.
+# https://postgrespro.com/list/thread-id/2448092
+
 PGPASSWORD="$PGPASSWORD" pg_dump \
   --column-inserts \
   --data-only \
@@ -20,6 +24,7 @@ PGPASSWORD="$PGPASSWORD" pg_dump \
   --host=db.wtefsnhgkloxbxhifnxp.supabase.co \
   --dbname=postgres \
   --username=postgres \
+  | grep -v "SELECT pg_catalog.set_config('search_path', '', false);" \
   > "$migrations_dir/$seed_filename"
 
 PGPASSWORD="$PGPASSWORD" pg_dump \
@@ -30,4 +35,5 @@ PGPASSWORD="$PGPASSWORD" pg_dump \
   --host=db.wtefsnhgkloxbxhifnxp.supabase.co \
   --dbname=postgres \
   --username=postgres \
+  | grep -v "SELECT pg_catalog.set_config('search_path', '', false);" \
   >> "$migrations_dir/$seed_filename"
